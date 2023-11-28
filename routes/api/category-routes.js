3649512c559e9c202c7bce1d5ab0b6f389196609
SlemJosh <joshua.slem@gmail.com>
2023-11-28 13:02:08 -0700

    Added a get route for searching by one id.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -20,9 +20,24 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'Category not found' });
+    } else {
+      res.status(200).json(categoryData);
+    }
+  }
+  catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
 });
 
 router.post('/', (req, res) => {
