41eaf8fb946a95217ad9dcdb33ba9b7b9e072bc2
SlemJosh <joshua.slem@gmail.com>
2023-11-28 10:52:47 -0700

    Defined sequelize for product tags.

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,40 +2,27 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      primaryKey: true,
+      primaryKeys: true,
       autoIncrement: true,
     },
-    // Defining the other columns
-    product_name: {
-      type: DataTypes.STRING,
-      allowNull: false, 
-    },
-    price: {
-      type: DataTypes.DECIMAL,
-      allowNull: false, 
-      validate: {
-        isDecimal: true,
-      },
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false, 
-      defaultValue: 10,
-      validate: {
-        isNumeric: true,
-      },
-    },
-    category_id: {
+    product_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'category',
+        model: 'product',
+        key: 'id'
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
         key: 'id',
       },
     },
