c26fe03f3d80f318d2e55679d11e10b2d289d647
SlemJosh <joshua.slem@gmail.com>
2023-11-28 10:30:13 -0700

    Defined sequelize for products

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,8 +6,41 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // Defining the other columns
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false, 
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false, 
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false, 
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
+
   {
     sequelize,
     timestamps: false,
