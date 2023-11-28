c7ccb64d64649e026cf97526fa6a991310bb5466
SlemJosh <joshua.slem@gmail.com>
2023-11-28 12:57:24 -0700

    Coded the get function for category routes.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,21 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+
+    if (!categoryData || categoryData.length === 0) {
+      return res.status(404).json({ message: 'No categories found' });
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
